c5ab3dedbde1c652b364b60f822e172784035370
AV <alenv983@gmail.com>
2024-06-21 15:57:25 +0530

    webite update

---
+++ test/navbar.php
@@ -0,0 +1,28 @@
+header>
+        <div class="logo">
+            Bug Free Happiness
+        </div>
+
+        <div class="hamburger">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+
+        <nav class="nav-bar">
+            <ul>
+                <li>
+                    <a href="" class="active">Home</a>
+                </li>
+                <li>
+                    <a href="https://github.com/Alen983/bug-free-happiness">Github</a>
+                </li>
+                <li>
+                    <a href="">Contribute</a>
+                </li>
+                <li>
+                    <a href="about.html">About</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
